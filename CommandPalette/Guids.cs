8398e8094c2c747ca2f5821e5b17aa1381df912e
ora <ora@outlook.com>
2014-09-14 21:06:06 -0700

    Initial commit

---
+++ CommandPalette/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace oradev.CommandPalette
+{
+    static class GuidList
+    {
+        public const string guidCommandPalettePkgString = "73e075c4-c2b8-4d9f-8eca-424494a01cca";
+        public const string guidCommandPaletteCmdSetString = "10d5c22f-ef51-44ec-bc57-cd31d8dcd9c2";
+
+        public static readonly Guid guidCommandPaletteCmdSet = new Guid(guidCommandPaletteCmdSetString);
+    };
+}
---
+++ CommandPalette/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Command Palette")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("oradev")]
+[assembly: AssemblyProduct("Command Palette")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("CommandPalette_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001007dc8f645f828078ddf1b5445eaeb134d45f6efc8b811c4e9fe70435e47afb6286b60fa20a52e8a9c32f5c3899df610f10b1a07c123c8765581d2991028d719ca33078e0df9c53e3ea3c11639d49a06caae2424d131d7bea39372f5b036752c27c9dd7466b87a53df7e7aefa50198fa2079c9988984a0e55627b14e0854af62b5")]
+[assembly: InternalsVisibleTo("CommandPalette_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001007dc8f645f828078ddf1b5445eaeb134d45f6efc8b811c4e9fe70435e47afb6286b60fa20a52e8a9c32f5c3899df610f10b1a07c123c8765581d2991028d719ca33078e0df9c53e3ea3c11639d49a06caae2424d131d7bea39372f5b036752c27c9dd7466b87a53df7e7aefa50198fa2079c9988984a0e55627b14e0854af62b5")]
